8e5fe642fb80d0c9f8eda43a948605cc07c0915d
Adarsh srivathsa M S <adarshsrivathsa1642000@gmail.com>
2023-07-09 23:33:04 +0530

    1 hello world

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello, World!";
+    }
+}
