6497e35766db23978402c66188201f12dc7d31dd
Adarsh srivathsa M S <adarshsrivathsa1642000@gmail.com>
2023-08-16 16:20:23 +0530

    Rotate matrix

---
+++ Arrays/RotateMatrix.java
@@ -0,0 +1,51 @@
+//Problem: Given a matrix, your task is to rotate the matrix 90 degrees clockwise.
+
+class RotateMatrix{
+
+    public static void main(String[] args) {
+        
+        int a[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println("Initial marix is ::");
+        for(int i=0; i< a.length; i++){
+            for(int j=0; j< a.length; j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+        rotate(a);
+        System.out.println("Rotated marix is ::");
+        for(int i=0; i< a.length; i++){
+            for(int j=0; j< a.length; j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static void rotate(int[][] a) {
+
+        for(int i = 0; i< a.length; i++){
+
+            for(int j=i; j<a[0].length; j++){
+
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+
+            }
+        }
+
+        for(int i=0; i<a.length; i++){
+
+            for(int j=0; j<a.length/2; j++){
+
+                int temp = a[i][j];
+                a[i][j] = a[i][a.length -1-j];
+                a[i][a.length -1-j] = temp;
+            }
+        }
+
+    }
+
+}
