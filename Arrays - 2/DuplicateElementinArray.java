3438f8ad549b2a2f8454f47dc09fa5f611de49f7
Adarsh srivathsa M S <adarshsrivathsa1642000@gmail.com>
2023-08-26 22:25:30 +0530

    Duplicate element in array

---
+++ Arrays - 2/DuplicateElementinArray.java
@@ -0,0 +1,24 @@
+//Problem: Given an array of N + 1 size, where each element is between 1 and N. Assuming there is only one duplicate number, your task is to find the duplicate number.
+
+
+class DuplicateElementinArray {
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+    public static void main(String args[]) {
+        int arr[] = {2,3,4,2,3};
+        System.out.println("The duplicate element is " + findDuplicate(arr));
+    }
+}
